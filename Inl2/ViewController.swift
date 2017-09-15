6a8a2ca5ef8a451923d0b0dacb4a82cd255af271
Yuliia Stelmakhovska <ystelmahovskaya@gmail.com>
2017-09-15 11:33:14 +0200

    Initial Commit

---
+++ Inl2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Inl2
+//
+//  Created by Yuliia Stelmakhovska on 2017-09-15.
+//  Copyright © 2017 Yuliia Stelmakhovska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
